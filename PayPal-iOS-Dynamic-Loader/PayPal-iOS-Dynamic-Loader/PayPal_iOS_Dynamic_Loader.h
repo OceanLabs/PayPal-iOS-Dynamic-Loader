72573fd11d127fc360610105a04b11a6dba0009e
Konstadinos Karayannis <kkarayannis@gmail.com>
2018-06-26 10:36:05 +0200

    Added support for Carthage

---
+++ PayPal-iOS-Dynamic-Loader/PayPal-iOS-Dynamic-Loader/PayPal_iOS_Dynamic_Loader.h
@@ -0,0 +1,20 @@
+//
+//  PayPal_iOS_Dynamic_Loader.h
+//  PayPal-iOS-Dynamic-Loader
+//
+//  Created by Konstadinos Karayannis on 26/06/2018.
+//  Copyright © 2018 Kite.ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PayPal_iOS_Dynamic_Loader.
+FOUNDATION_EXPORT double PayPal_iOS_Dynamic_LoaderVersionNumber;
+
+//! Project version string for PayPal_iOS_Dynamic_Loader.
+FOUNDATION_EXPORT const unsigned char PayPal_iOS_Dynamic_LoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayPal_iOS_Dynamic_Loader/PublicHeader.h>
+
+#import <PayPal_iOS_Dynamic_Loader/OLPayPalWrapper.h>
+
